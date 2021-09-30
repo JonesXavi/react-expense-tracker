d78d713199e09b53f4cf2497bcd4b48c5940a5db
Jones Bastin Xavier <jonesedvin@gmail.com>
2021-09-30 12:57:49 +0530

    Updated css and added transaction type

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -6,17 +6,28 @@ import { GlobalContext } from '../context/GlobalState';
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
+  const [transactionType, setTransactionType] = useState('income');
   const { addTransaction } = useContext(GlobalContext);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
+    let tempAmount = 0;
+
+    if(transactionType === 'income') {
+      tempAmount = '+'+ amount;
+    } else {
+      tempAmount = '-'+ amount;
+    }
+
     const newTransaction = {
       id: uuidv4(),
+      transactionType,
       text,
-      amount: +amount
+      amount: +tempAmount
     }
     addTransaction(newTransaction);
+    setTransactionType('income');
     setText('');
     setAmount(0);
   };
@@ -25,14 +36,24 @@ export const AddTransaction = () => {
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
+          <div>Type</div>
+          <label htmlFor="income" className="form-label">
+            <input id="income" value="income" name="transactionType" label="Income" type="radio" onChange={(e) => setTransactionType(e.target.value)} checked={transactionType === 'income'} className="form-label" />
+            Income
+          </label>
+          <label htmlFor="expense">
+            <input id="expense" value="expense" name="transactionType" label="Expense" type="radio" onChange={(e) => setTransactionType(e.target.value)} checked={transactionType === 'expense'} className="form-label" />
+            Expense
+          </label>
+        </div>
+        <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
-            (negative - expense, positive - income)</label
-          >
+          </label>
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -3,15 +3,15 @@ import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
   const { transactions, deleteTransaction } = useContext(GlobalContext);
-  console.log(transactions);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
         { transactions.length > 0 ?
           transactions.map(transaction => (
-            <li className={transaction.amount > 0 ? 'plus' : 'minus'} key={transaction.id}>
-              {transaction.text} <span>{transaction.amount > 0 ? '+' : '-'} &#x20B9; {Math.abs(transaction.amount)}</span>
+            <li className={transaction.transactionType === 'income' ? 'plus' : 'minus'} key={transaction.id}>
+              {transaction.text} <span>{transaction.transactionType === 'income' ? '+' : '-'} &#x20B9; {Math.abs(transaction.amount)}</span>
               <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
             </li>
           )) : <div>No Transactions Available</div>
